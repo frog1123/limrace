05df97c727301f742c2228b0a6fc657674ac4545
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-29 17:33:30 -0600

    +

--- client/index.js
+++ client/index.js
@@ -7,10 +7,36 @@ socket.on("connect", () => {
 
 socket.on("user-connected", name => {
   document.getElementById("login-info").textContent = `Logged in as ${name}`;
+
+  race.innerHTML = `${race.innerHTML}
+  <div class="row">
+    <div class="track">
+      <span>🏎️</span>
+    </div>
+    <div>
+      <span>0 wpm</span>
+      <span>0:00</span>
+    </div>
+  </div>
+  <div class="separator"></div>`;
 });
 
 socket.on("broadcasted-user-connected", name => {
   document.getElementById("other-users").textContent = `${document.getElementById("other-users").textContent} ${name}`;
+
+  const race = document.getElementById("race");
+
+  race.innerHTML = `${race.innerHTML}
+  <div class="row">
+    <div class="track">
+      <span>🏎️</span>
+    </div>
+    <div>
+      <span>0 wpm</span>
+      <span>0:00</span>
+    </div>
+  </div>
+  <div class="separator"></div>`;
 });
 
 // socket.on("user-disconnected", name => {
