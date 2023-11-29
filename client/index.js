c3fdbb9e21cead58b49f6d77190634279d0164d5
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-11-29 17:17:27 -0600

    +

--- client/index.js
+++ client/index.js
@@ -1,34 +1,35 @@
-const socket = io('http://localhost:3000');
-const messageContainer = document.getElementById('message-container');
-const messageForm = document.getElementById('send-container');
-const messageInput = document.getElementById('message-input');
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+  console.log("connected to server");
+  socket.emit("new-user", "guestasd");
+});
 
 // const name = prompt('What is your name?');
-appendMessage('You joined');
-socket.emit('new-user', name);
+// appendMessage("You joined");
 
-socket.on('chat-message', data => {
-  appendMessage(`${data.name}: ${data.message}`);
+// socket.on("chat-message", data => {
+//   appendMessage(`${data.name}: ${data.message}`);
+// });
+
+socket.on("user-connected", name => {
+  document.getElementById("login-info").textContent = `Logged in as ${name}`;
 });
 
-socket.on('user-connected', name => {
-  appendMessage(`${name} connected`);
-});
+// socket.on("user-disconnected", name => {
+//   appendMessage(`${name} disconnected`);
+// });
 
-socket.on('user-disconnected', name => {
-  appendMessage(`${name} disconnected`);
-});
+// messageForm.addEventListener("submit", e => {
+//   e.preventDefault();
+//   const message = messageInput.value;
+//   appendMessage(`You: ${message}`);
+//   socket.emit("send-chat-message", message);
+//   messageInput.value = "";
+// });
 
-messageForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const message = messageInput.value;
-  appendMessage(`You: ${message}`);
-  socket.emit('send-chat-message', message);
-  messageInput.value = '';
-});
-
-function appendMessage(message) {
-  const messageElement = document.createElement('div');
-  messageElement.innerText = message;
-  messageContainer.append(messageElement);
-}
+// function appendMessage(message) {
+//   const messageElement = document.createElement("div");
+//   messageElement.innerText = message;
+//   messageContainer.append(messageElement);
+// }
--- server/index.js
+++ server/index.js
@@ -1,21 +1,25 @@
-const io = require('socket.io')(3000, {
+const io = require("socket.io")(3000, {
   cors: {
-    origin: '*'
+    origin: "*"
   }
 });
 
 const users = {};
 
-io.on('connection', socket => {
-  socket.on('new-user', name => {
+io.on("connection", socket => {
+  socket.on("new-user", name => {
+    console.log("new user connected");
+    console.log("users", users);
     users[socket.id] = name;
-    socket.broadcast.emit('user-connected', name);
+    socket.emit("user-connected", name);
   });
-  socket.on('send-chat-message', message => {
-    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] });
+
+  socket.on("send-chat-message", message => {
+    socket.broadcast.emit("chat-message", { message: message, name: users[socket.id] });
   });
-  socket.on('disconnect', () => {
-    socket.broadcast.emit('user-disconnected', users[socket.id]);
+
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
     delete users[socket.id];
   });
 });
