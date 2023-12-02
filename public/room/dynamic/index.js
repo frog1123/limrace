7d1edb89a48470bfa6dec64a7e89734f2c45da45
frog1123 <93895449+frog1123@users.noreply.github.com>
2023-12-02 11:45:21 -0600

    +

--- public/room/dynamic/index.js
+++ public/room/dynamic/index.js
@@ -8,10 +8,13 @@ socket.on("connect", () => {
 });
 
 let roomUsers = [];
+let currentWord;
+
 socket.on("user-connected", data => {
   document.getElementById("login-info").textContent = `logged in as ${data.name} in room ${data.room.id}`;
 
   renderInitialText(data.room.text);
+  currentWord - data.room.text.split(/\s+/)[0];
 
   roomUsers = data.users;
   renderUsers();
@@ -49,5 +52,17 @@ const renderUsers = () => {
 };
 
 const renderInitialText = text => {
-  document.getElementById("text").textContent = text;
+  const container = document.getElementById("text");
+  const words = text.split(/\s+/);
+
+  words.forEach(word => {
+    const span = document.createElement("span");
+    span.textContent = word + " ";
+    container.appendChild(span);
+  });
 };
+
+const input = document.getElementById("input");
+input.addEventListener("input", () => {
+  console.log("Input value changed:", input.value);
+});
